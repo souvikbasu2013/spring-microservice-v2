b9d7c4517ba4b9f78fdaaffbb5125b24478e9e06
souvikbasu2013 <bunty.nil@gmail.com>
2023-04-01 02:04:33 +0530

    currency exchange service

--- currency-exchange-service/src/main/java/com/tiptostartmycode/restservices/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/tiptostartmycode/restservices/controller/CurrencyExchangeController.java
@@ -1,24 +1,48 @@
 package com.tiptostartmycode.restservices.controller;
 
-import java.math.BigDecimal;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tiptostartmycode.restservices.bean.CurrencyExchange;
+import com.tiptostartmycode.restservices.repository.CurrencyExchangeRepository;
 
 @RestController
 public class CurrencyExchangeController {
 	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	@Value("${server.port}")
 	String port;
+	
+	@Autowired
+	private CurrencyExchangeRepository repository;
+	
+	@Autowired
+	private Environment environment;
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange getExchangeValue(@PathVariable String from, @PathVariable String to) {
 		
-		return new CurrencyExchange(1000L, "USD", "INR", BigDecimal.valueOf(80.0),port);
+		logger.info("retrieveExchangeValue called with {} to {}", from, to);
+		
+		CurrencyExchange currencyExchange 
+					= repository.findByFromAndTo(from, to);
+		
+		if(currencyExchange ==null) {
+			throw new RuntimeException
+				("Unable to Find data for " + from + " to " + to);
+		}
+		
+		String port = environment.getProperty("local.server.port");
+		currencyExchange.setEnvironment(port);
+		
+		return currencyExchange;
 		
 	}
 
